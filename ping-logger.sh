3ed570e6ccc8fc30c784dd5a2c9630e15fbf3e79
Balazs Zsido <balazs.zsido@gmail.com>
2020-06-07 16:55:08 +0100

    added deletion

--- ping-logger.sh
+++ ping-logger.sh
@@ -5,9 +5,13 @@ T=85400
 # interval to wait between pings
 I=5
 # logfile location
-LOG="/var/log/pinglog/pinglog-$(date +%F)"
+LOC="/var/log/pinglog"
+LOG="$LOC/pinglog-$(date +%F)"
 
 ping 8.8.8.8 -i "$I" -w "$T" | while read PONG
 do
     echo "$(date): $PONG" >> "$LOG"
 done
+
+# delete logs older than 6 months
+find $LOC -mtime +186 -exec rm {} \;
